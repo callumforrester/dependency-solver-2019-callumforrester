89b971664b8bb1818f7aca21c7c442a647802141
Callum Forrester <caf32@kent.ac.uk>
2019-02-05 12:18:15 +0000

    Completed initial loading of JSON data

---
+++ src/package.py
@@ -0,0 +1,32 @@
+import json
+
+from typing import List, Dict, Iterator
+from dataclasses import dataclass
+
+
+# Typedef
+Repository = Dict[str, 'Package']
+
+
+@dataclass
+class Package:
+    name: str
+    version: str
+    size: int
+    dependencies: List[List[str]]
+    conflicts: List[str]
+
+
+def parse(d: Dict, r: Repository = None) -> Package:
+    return Package(
+        d['name'],
+        d['version'],
+        d['size'],
+        d['dependencies'] if 'dependencies' in d else [],
+        d['conflicts'] if 'conflicts' in d else []
+    )
+
+
+def load_dict(file_path: str) -> Iterator[Dict]:
+    with open(file_path, 'r') as handle:
+        return json.load(handle)
