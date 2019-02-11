bbb336f247f422358ade8da0c7bd8d8e5d9726e9
Callum Forrester <caf32@kent.ac.uk>
2019-02-11 19:02:58 +0000

    Changed parser to store package inequalities as is, preprocessing will happen later or possibly not at all

--- src/compare.py
+++ src/compare.py
@@ -1,6 +1,4 @@
-from typing import TypeVar, Callable, List
-
-from src.package import Package, Repository, Constraints
+from typing import TypeVar, Callable
 
 
 GREATER = '>'
@@ -10,75 +8,6 @@ LESS_OR_EQUAL = '<='
 LESS = '<'
 
 # Deps format: [[A OR B] AND [C]]
-
-
-def parse_constraints(repository: Repository,
-                      constraints: List[str]) -> Constraints:
-    dependencies = []
-    conflicts = []
-    for constraint in constraints:
-        command = constraint[0]
-        reference = constraint[1:]
-
-        packages = flatten_reference(repository, reference)
-
-        if command == '+':
-            dependencies += [packages]
-        elif command == '-':
-            conflicts.append(packages)
-        else:
-            raise ValueError(
-                    'Attempted to parse invalid constraint: %s' % constraint)
-
-    return Constraints(dependencies, conflicts)
-
-
-# TODO: Make this less tight-looped
-def flatten_repository(repository: Repository) -> Repository:
-    for package, relations in repository.items():
-        dependencies = []
-        conflicts = []
-        for dependency_disjunct in relations.dependencies:
-            new_disjunct = []
-            for dependency in dependency_disjunct:
-                new_disjunct += flatten_reference(repository, dependency)
-            dependencies.append(new_disjunct)
-        for conflict in relations.conflicts:
-            conflicts += flatten_reference(repository, conflict)
-        repository[package] = Constraints(
-            dependencies,
-            conflicts
-        )
-    return repository
-
-
-def flatten_reference(repo: Repository, reference: str) -> List[Package]:
-    check = filter_for_operator(reference)
-    return list(filter(check, repo.keys()))
-
-
-def filter_for_operator(reference: str) -> Callable[[Package], bool]:
-    if GREATER_OR_EQUAL in reference:
-        return filter_for(reference, GREATER_OR_EQUAL)
-    elif GREATER in reference:
-        return filter_for(reference, GREATER)
-    elif LESS_OR_EQUAL in reference:
-        return filter_for(reference, LESS_OR_EQUAL)
-    elif LESS in reference:
-        return filter_for(reference, LESS)
-    elif EQUAL in reference:
-        return filter_for(reference, EQUAL)
-    else:
-        return lambda p: p.name == reference
-
-
-def filter_for(reference: str, operator: str) -> Callable[[Package], bool]:
-    args = reference.split(operator)
-    name = args[0]
-    version = args[1]
-
-    cmp = compare(operator)
-    return lambda p: p.name == name and cmp(p.version, version)
 
 
 TComparable = TypeVar('TComparable')
--- src/package.py
+++ src/package.py
@@ -1,45 +1,101 @@
 import json
+import re
 
-from typing import List, Dict, Iterator
+from typing import List, Dict, Iterator, Iterable, Callable
 from dataclasses import dataclass
+from functools import partial
+
+from src.compare import compare
 
 
-@dataclass(eq=True, frozen=True)
-class Package:
-    name: str
-    version: str
-    size: int
+Version = List[int]
+
+
+PACKAGE_REFERENCE_REGEX = '([.+a-zA-Z0-9-]+)(?:(>=|<=|=|<|>)(\d+(?:\.\d+)*))?'
+COMMAND_REGEX = '([+-])%s' % PACKAGE_REFERENCE_REGEX
+
+
+old_map = map
+map = lambda fn, it: list(old_map(fn, it))
 
 
 @dataclass
-class Constraints:
-    dependencies: List[List[Package]]
-    conflicts: List[Package]
+class PackageReference:
+    name: str
+    version: Version
+    compare: Callable[[Version], bool]
 
 
-Repository = Dict[Package, Constraints]
+@dataclass
+class Command:
+    plus_minus: str
+    reference: PackageReference
+
+
+@dataclass
+class Package:
+    name: str
+    version: Version
+    size: int
+    dependencies: List[List[PackageReference]]
+    conflicts: List[PackageReference]
+
+
+Repository = Iterable[Package]
 
 
 def parse_repository(repository: List[Dict]) -> Repository:
-    return {
-        parse_package(d): parse_package_constraints(d)
-        for d in repository
-    }
+    return map(parse_package, repository)
 
 
 def parse_package(d: Dict) -> Package:
     return Package(
         d['name'],
-        d['version'],
-        d['size']
+        parse_version(d['version']),
+        d['size'],
+        parse_dependencies(d['depends']) if 'depends' in d else [],
+        parse_package_references(d['conflicts']) if 'conflicts' in d else []
     )
 
 
-def parse_package_constraints(d: Dict) -> Constraints:
-    return Constraints(
-        d['depends'] if 'depends' in d else [],
-        d['conflicts'] if 'conflicts' in d else []
-    )
+def parse_version(version: str) -> Version:
+    return list(map(int, version.split('.')))
+
+
+def parse_dependencies(dependencies: List[List[str]]) -> Iterable[Iterable[
+                                                         PackageReference]]:
+    return map(parse_package_references, dependencies)
+
+
+def parse_package_references(references: List[str]) -> Iterable[
+                                                        PackageReference]:
+    return map(parse_package_reference, references)
+
+
+def parse_package_reference(reference: str) -> PackageReference:
+        name, operator, version = re\
+                                    .compile(PACKAGE_REFERENCE_REGEX)\
+                                    .match(reference)\
+                                    .groups()
+        return make_package_reference(name, version, operator)
+
+
+def parse_command_list(commands: List[str]) -> Iterable[Command]:
+    return map(parse_command, commands)
+
+
+def parse_command(command: str) -> Command:
+    plus_minus, name, operator, version = re.compile(COMMAND_REGEX)\
+                                            .match(command)\
+                                            .groups()
+    return Command(plus_minus, make_package_reference(name, version, operator))
+
+
+def make_package_reference(name, version, operator) -> PackageReference:
+            parsed_version = parse_version(version) if version else None
+            return PackageReference(name, parsed_version,
+                                    partial(compare(operator), parsed_version)
+                                    if operator else None)
 
 
 def load_dict(file_path: str) -> Iterator[Dict]:
