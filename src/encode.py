fc02359282b69f3df9536ebc48cb6d3913229a60
Callum Forrester <caf32@kent.ac.uk>
2019-02-13 14:05:22 +0000

    Added constraint on package delta between states

--- src/encode.py
+++ src/encode.py
@@ -1,6 +1,8 @@
-from z3 import Optimize, And, Not, Or, Bool, Implies, Z3PPObject
+import itertools
 
-from typing import Iterable, Callable, TypeVar
+from z3 import Optimize, And, Not, Or, Bool, Implies, Z3PPObject, If, Sum
+
+from typing import Iterable, Callable, TypeVar, Tuple
 
 from src.package import Package, Command, CommandSort, PackageGroup, Constraint, PackageIdentifier
 from src.expand import expand_repository, expand_commands
@@ -21,11 +23,36 @@ def encode(repository: Iterable[Package],
     for step in range(GUESS_STEPS):
         formula = And(constrain_repository(repository, step),
                       constrain_commands(final_state_constraints),
-                      constrain_initial_state(initial_state))
+                      constrain_initial_state(initial_state),
+                      constrain_all_delta(repository, range(GUESS_STEPS)))
         s.add(formula)
 
     print(s)
     return s
+
+
+def constrain_all_delta(repository: Iterable[Package],
+                        time_steps: Iterable[int]) -> And:
+    return And([constrain_delta(repository, f, t)
+                for f, t in neighbours(time_steps)])
+
+
+TNeighbour = TypeVar('TNeighbour')
+
+
+def neighbours(it: Iterable[TNeighbour]) -> Iterable[
+                                            Tuple[TNeighbour, TNeighbour]]:
+    return zip(it, itertools.islice(it, 1, None))
+
+
+def constrain_delta(repository, from_time, to_time):
+    return Sum([delta(to_bool(p.identifier, from_time),
+                      to_bool(p.identifier, to_time))
+                for p in repository]) <= 1
+
+
+def delta(b0: Bool, b1: Bool) -> If:
+    return If(b0 == b1, 0, 1)
 
 
 def constrain_initial_state(initial_state: Iterable[Package]) -> And:
@@ -58,6 +85,7 @@ def constrain_commands(commands: Iterable[Command]) -> And:
 
 
 def command_to_bool(command: Command) -> Bool:
+    # TODO: Don't use constant
     return {
         CommandSort.INSTALL: require(command.reference, GUESS_STEPS - 1),
         CommandSort.UNINSTALL: Not(require(command.reference, GUESS_STEPS - 1))
