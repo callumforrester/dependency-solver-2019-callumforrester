139aab254986948ffcbf83c7a0133d0343a82b29
Callum Forrester <caf32@kent.ac.uk>
2019-02-19 22:10:54 +0000

    Forgot to create virtualenv

--- install_deps.sh
+++ install_deps.sh
@@ -5,5 +5,6 @@ add-apt-repository -y ppa:deadsnakes/ppa
 apt-get update
 apt-get install -y python3.7 python-virtualenv
 
+virtualenv -p /usr/bin/python3.7 --no-site-packages venv
 . venv/bin/activate
 pip install -r requirements.txt
