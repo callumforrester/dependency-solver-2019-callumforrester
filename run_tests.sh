207b78380aaa5b868b36c9405e99e649b29ee327
Callum Forrester <caf32@kent.ac.uk>
2019-02-19 17:41:16 +0000

    Modified run_tests.sh to run a single test if passed as an argument

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-for f in $(ls -d tests/*); do
+if [ $# == 1 ]; then
+    TESTS=$1
+else
+    TESTS=$(ls -d tests/*)
+fi
+
+for f in $TESTS; do
   echo "Running $f"
   ./solve $f/repository.json $f/initial.json $f/constraints.json
 done
