a5b14ec77018dd3d3777830d586ecd1bc2cdf80d
Callum Forrester <caf32@kent.ac.uk>
2019-02-19 22:09:57 +0000

    Modifed venv settings to work with docker container

--- install_deps.sh
+++ install_deps.sh
@@ -2,6 +2,8 @@
 apt-get update
 apt-get install -y software-properties-common
 add-apt-repository -y ppa:deadsnakes/ppa
-apt-get install -y python3.7
+apt-get update
+apt-get install -y python3.7 python-virtualenv
 
+. venv/bin/activate
 pip install -r requirements.txt
