86b129561d88de05d87a876b74bfbac0e9abc7b4
Callum Forrester <caf32@kent.ac.uk>
2019-02-25 13:36:13 +0000

    Fixed bug where non-existant packages were not always ignored

--- solver/expand/package.py
+++ solver/expand/package.py
@@ -1,4 +1,5 @@
 from functools import partial
+from typing import List
 
 from solver.package.package import Package, PackageReference, PackageGroup, Repository
 from solver.flatten import flatten_as_list
@@ -14,10 +15,17 @@ def expand_repository(repository: Repository) -> PackageGroup:
 
 def expand_package(repository: Repository, package: Package) -> Package:
     expand = partial(expand_reference, repository)
-    expanded_dependencies = list(map(lambda d: flatten_as_list(map(expand, d)),
-                                     package.dependencies))
+    expanded_dependencies = expand_dependencies(repository,
+                                                package.dependencies)
     expanded_conflicts = flatten_as_list(map(expand, package.conflicts))
     return Package(package.size, expanded_dependencies, expanded_conflicts)
+
+
+def expand_dependencies(repository: Repository,
+                        dependencies: List[List[PackageReference]]) -> List[List[PackageGroup]]:
+    expand = partial(expand_reference, repository)
+    return list(filter(bool, map(lambda d: flatten_as_list(map(expand, d)),
+                                                           dependencies)))
 
 
 def expand_reference(repository: Repository,
