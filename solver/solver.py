dbb5f5b0026c04a22365a30054a8780d270310d7
Callum Forrester <caf32@kent.ac.uk>
2019-03-04 12:17:45 +0000

    Added timeout to z3

--- solver/solver.py
+++ solver/solver.py
@@ -9,9 +9,12 @@ DEFAULT_TIME_STEPS = 100
 
 
 def solve(problem: DependencyProblem,
-          time_steps=DEFAULT_TIME_STEPS) -> Optional[List[Command]]:
+          time_steps: int = DEFAULT_TIME_STEPS,
+          timeout_ms: Optional[int] = None) -> Optional[List[Command]]:
     states = generate_state_map(problem.repository, range(time_steps))
     z3_problem = to_z3_problem(problem, states)
+    if timeout_ms is not None:
+        z3_problem.set('timeout', timeout_ms)
     if z3_problem.check():
         return decode(z3_problem.model(), states)
     else:
