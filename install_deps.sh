87c02a00b5c82b88b9d873f286dd730640c9b878
Callum Forrester <callum.forrester96@gmail.com>
2019-11-27 20:51:21 +0000

    Switched to Pipfile

--- install_deps.sh
+++ install_deps.sh
@@ -5,6 +5,4 @@ add-apt-repository -y ppa:deadsnakes/ppa
 apt-get update
 apt-get install -y python3.7 python-virtualenv
 
-virtualenv -p /usr/bin/python3.7 --no-site-packages venv
-. venv/bin/activate
-pip install -r requirements.txt
+pipenv install
